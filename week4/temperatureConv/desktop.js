17f63e049d6edf59cf60060ba48c66f021d9d116
Gael <wapinpana@gmail.com>
2015-07-31 21:12:08 -0400

    reset button working and margin adj.

--- week4/temperatureConv/desktop.js
+++ week4/temperatureConv/desktop.js
@@ -8,7 +8,7 @@ document.getElementById("clickme2").onclick = calculateCel;
 function calculateFah () {
 	celNumber = document.getElementById("celNumber").value;
 
-	var convertTempFah = celNumber * 1.8 + 32 + "  Fahrenheit";
+	var convertTempFah = (celNumber * 1.8 + 32).toFixed(2) + "  Fahrenheit";
 	document.getElementById("convertTempFah").innerHTML = convertTempFah;
 
 }
@@ -16,11 +16,14 @@ function calculateFah () {
 function calculateCel () {
 	fahNumber = document.getElementById("fahNumber").value; 
 
-	var convertTempCel = (fahNumber - 32) / 1.8 + "  Celcius";
+	var convertTempCel = Math.round(((fahNumber - 32) / 1.8), 2) + "  Celcius";
 	document.getElementById("convertTempCel").innerHTML = convertTempCel;
 
 }
 
-document.getElementById("reset").onclick = function() {
-   document.getElementById("celNumber").innerHTML = " ";
+   document.getElementById("reset").onclick = function() {
+   document.getElementById("celNumber").value = "";
+   document.getElementById("fahNumber").value = "";
+   document.getElementById("convertTempCel").innerHTML = "";
+   document.getElementById("convertTempFah").innerHTML = "";
 };
