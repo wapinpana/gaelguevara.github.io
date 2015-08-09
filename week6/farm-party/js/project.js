daa4e1562f574072508dfe58bc881f5f6e89e048
Gael <wapinpana@gmail.com>
2015-08-08 22:53:22 -0400

    start of farm partaaaay

--- week6/farm-party/js/project.js
+++ week6/farm-party/js/project.js
@@ -1,4 +1,31 @@
 $(document).ready(function(){
+	var signOn = true;
+	function changeColor () {
+	if(signOn == true){
+		$('h1').css("color", "#e74c3c");
+		signOn = false;
+	} else if (signOn == false){
+		$(h1).css("color", "#9b59b6");
+		signOn = true; 
+	}
+}
 
+setInterval("changeColor()", 400);
 
 });
+
+
+
+// /////////////////////////////////////////////////
+// var flag = true;
+// function changeColor () {
+//     if(flag==true){
+//         document.getElementById("yourId").style.background="yourColor 1";
+//         flag=false;
+//     }
+//     else if (flag==false){
+//     document.getElementById("yourId").style.background="yourcolor 2";
+//     flag = true;
+//     }
+// }
+// setInterval("changeColor()", timeinmillisec);
