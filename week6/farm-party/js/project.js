2a1c078d7b1636f142e69c847bd82f20b43cc1bb
Gael <wapinpana@gmail.com>
2015-08-10 22:32:59 -0400

    farm party got some animals to move

--- week6/farm-party/js/project.js
+++ week6/farm-party/js/project.js
@@ -1,20 +1,45 @@
 $(document).ready(function(){
-	var signOn = true;
-	function changeColor () {
-	if(signOn == true){
-		$('h1').css("color", "#e74c3c");
-		signOn = false;
-	} else if (signOn == false){
-		$(h1).css("color", "#9b59b6");
-		signOn = true; 
-	}
-}
 
-setInterval("changeColor()", 400);
+	$("button").click(function() {
+
+	  	$("#roost").animate({"left" : "900px" }, "fast");
+
+		$("#dancing").animate({"left" : "100px" }, "100s");
+
+		$("#cow1").animate({"top" : "100px"}, "slow");
+
+		$("#cow2").animate({
+            height: 'toggle'
+        });
+
+		$("#sun").animate({
+    		"top": "100%",
+    		"left": "100%"
+  			}, 2000);
+	});
+
+	// $("div").hover(function(){
+	// 	$("#horse").animate({
+	// 		"top" : 50%;
+	// 		"left" : 100%;
+	// 	}, 2000);
+
+	// });
 
 });
 
+	
 
+
+
+// $("a").click(function() {
+//   var num = $(this).index();
+//   $("div").eq(num).css("background-color", "red");
+//   $("div").eq(num).animate({
+//     "height": "70px", 
+//     "width": "20px"
+//   }, 200);
+// });
 
 // /////////////////////////////////////////////////
 // var flag = true;
