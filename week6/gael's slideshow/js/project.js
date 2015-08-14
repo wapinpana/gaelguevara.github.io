c7236552673ad1a3505aa71f18026379061a346f
Gael <wapinpana@gmail.com>
2015-08-14 16:42:36 -0400

    changing images but
    changing all of them have to limit the change to just the next one

--- week6/gael's slideshow/js/project.js
+++ week6/gael's slideshow/js/project.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
+	// var current = $(".current slide");
+	// $current.src($(this).attr("src");
+
 	$(".next").click(function(){
 		$("img").removeClass();
 		$("img").addClass("current slide");
-		var nextSlide = $(this).index();
+		$("img").attr("src", "images/3.jpg");
+		var nextSlide = $(this).next();
 		$("img.current slide").fadeOut(200, function(){
 			$("img.current slide").removeClass("current slide");
 			$("img").eq(nextSlide).show().addClass("current slide");
