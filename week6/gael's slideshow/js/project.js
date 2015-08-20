b02dc564ea6e76088d2098ea85df0288378c778e
Gael <wapinpana@gmail.com>
2015-08-20 11:39:55 -0400

    upload of startup match assignment and watermanpro website updates

--- week6/gael's slideshow/js/project.js
+++ week6/gael's slideshow/js/project.js
@@ -1,18 +1,20 @@
-$(document).ready(function(){
-	// var current = $(".current slide");
-	// $current.src($(this).attr("src");
-
-	$(".next").click(function(){
-		$("img").removeClass();
-		$("img").addClass("current slide");
-		$("img").attr("src", "images/3.jpg");
-		var nextSlide = $(this).next();
-		$("img.current slide").fadeOut(200, function(){
-			$("img.current slide").removeClass("current slide");
-			$("img").eq(nextSlide).show().addClass("current slide");
-		});
-	});
-});
+// $(document).ready(function(){
+// 	// var current = $(".current slide");
+// 	// $current.src($(this).attr("src");
+// 		// var current = $(".current_slide");
+// 		// $current.attr("src", "images/1.jpg");
+// 	$(".next").click(function(){
+// 		// $current = $current.next();
+// 		$("img").removeClass();
+// 		$("img").next().addClass("current_slide");
+// 		$("img").attr("src", "images/3.jpg");
+// 		var nextSlide = $(this).next();
+// 		$("img.current slide").fadeOut(200, function(){
+// 			$("img.current slide").removeClass("current slide");
+// 			$("img").eq(nextSlide).show().addClass("current slide");
+// 		});
+// 	});
+// });
 
 // 	$(".prev").click(function(){
 // 		$(this).removeClass();
@@ -20,3 +22,20 @@ $(document).ready(function(){
 
 // 	});
 // });
+
+$(document).ready(function(){
+	$(".next").click(function(){
+		$(".current_slide").removeClass("current_slide").next().addClass("current_slide");
+		// if($(".current_slide").is(":last-of-type")) {
+			
+
+		// }
+	});
+
+	$(".prev").click(function(){
+		$(".current_slide").removeClass("current_slide").prev().addClass("current_slide");
+	// 	// if($(".current_slide").is(":last-of-type")) {
+	});
+
+
+});
